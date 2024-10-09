083c5a7a9e1cd3991cdd970855dc7170fb681949
AGUILAR <93629269+ARMALITES@users.noreply.github.com>
2024-10-09 10:55:18 +0800

    Announcement Controller

---
+++ ASI.Basecode.WebApp/Controllers/AnnouncementController.cs
@@ -0,0 +1,21 @@
+﻿using ASI.Basecode.WebApp.Mvc;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace ASI.Basecode.WebApp.Controllers
+{
+	public class AnnouncementController : ControllerBase<AnnouncementController>
+	{
+		public AnnouncementController(IHttpContextAccessor httpContextAccessor, ILoggerFactory loggerFactory, IConfiguration configuration, IMapper mapper = null) : base(httpContextAccessor, loggerFactory, configuration, mapper)
+		{
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
