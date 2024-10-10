5b83cc5859ffa3d1083a6b7b4dba58051079966f
Rey Aaron Buaya <reycrocs@gmail.com>
2024-10-10 19:25:11 +0800

    feedback module

---
+++ ASI.Basecode.Services/Interfaces/IFeedbackService.cs
@@ -0,0 +1,20 @@
+﻿using ASI.Basecode.Services.ServiceModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASI.Basecode.Services.Interfaces
+{
+    public interface IFeedbackService
+    {
+        List<FeedbackViewModel> GetListOfFeedbacks();
+
+        void AddFeedback(FeedbackViewModel model);
+
+        void EditFeedback(FeedbackViewModel model);
+
+        void DeleteFeedback(int feedbackId);
+    }
+}
---
+++ ASI.Basecode.Services/ServiceModels/FeedbackViewModel.cs
@@ -0,0 +1,26 @@
+﻿using ASI.Basecode.Data.Models;
+using ASI.Basecode.Services.Validations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASI.Basecode.Services.ServiceModels
+{
+    public class FeedbackViewModel
+    {
+        public int FeedbackId { get; set; }
+        public int TicketId { get; set; }
+        public string UserId { get; set; }
+
+        [Required(ErrorMessage = "Comment is required.")]
+        public string Comment { get; set; }
+
+        [Required(ErrorMessage = "Rating is required.")]
+        public int Rating { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedTime { get; set; }
+    }
+}
