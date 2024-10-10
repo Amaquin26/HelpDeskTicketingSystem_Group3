2cbb6bb196a70aa9024adbcc936a167718edec09
Jermamae Aguilar <jermaineaguilar4@gmail.com>
2024-10-10 21:27:12 +0800

    announcement module

---
+++ ASI.Basecode.Services/ServiceModels/AnnouncementViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASI.Basecode.Services.ServiceModels
+{
+    public class AnnouncementViewModel
+    {
+        public int AnnouncementId { get; set; }
+
+        [Required(ErrorMessage = "Title is required.")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Content is required.")]
+        public string Content { get; set; }
+
+        public string CreatedBy { get; set; }
+        public DateTime CreatedTime { get; set; }
+        public string? UpdatedBy { get; set; }
+        public DateTime? UpdatedTime { get; set; }
+    }
+}
