36c25e703a9ce967ddc2db75c15d8a4f4a17f768
Perod122 <jasperballesteros21@gmail.com>
2024-10-12 13:52:15 +0800

    XLM documents for ITeamRepo and Team Repo

--- ASI.Basecode.Data/Interfaces/ITeamRepository.cs
+++ ASI.Basecode.Data/Interfaces/ITeamRepository.cs
@@ -9,12 +9,46 @@ namespace ASI.Basecode.Data.Interfaces
 {
     public interface ITeamRepository
     {
+        /// <summary>
+        /// Retrieves all teams from the database.
+        /// </summary>
+        /// <returns>
+        /// An <see cref="IQueryable{Team}"/> representing the collection of all teams.
+        /// </returns>
         IQueryable<Team> GetTeams();
+
+        /// <summary>
+        /// Retrieves a specific team by its unique identifier.
+        /// </summary>
+        /// <param name="teamId">The unique identifier of the team.</param>
+        /// <returns>
+        /// A <see cref="Team"/> object representing the team with the specified <paramref name="teamId"/>.
+        /// Returns <c>null</c> if the team is not found.
+        /// </returns>
         Team GetTeamById(int teamId);
+
+        /// <summary>
+        /// Adds a new team to the database.
+        /// </summary>
+        /// <param name="team">The <see cref="Team"/> object representing the new team to be added.</param>
         void AddTeam(Team team);
+
+        /// <summary>
+        /// Updates the details of an existing team.
+        /// </summary>
+        /// <param name="team">The <see cref="Team"/> object containing updated information.</param>
         void UpdateTeam(Team team);
+
+        /// <summary>
+        /// Deletes a team from the database.
+        /// </summary>
+        /// <param name="teamId">The unique identifier of the team to be deleted.</param>
         void DeleteTeam(int teamId);
+
+        /// <summary>
+        /// Saves all changes made to the repository.
+        /// </summary>
         void SaveTeam();
     }
-
 }
+
--- ASI.Basecode.Data/Repositories/TeamRepository.cs
+++ ASI.Basecode.Data/Repositories/TeamRepository.cs
@@ -12,10 +12,21 @@ namespace ASI.Basecode.Data.Repositories
 {
     public class TeamRepository : BaseRepository, ITeamRepository
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TeamRepository"/> class with the specified <see cref="IUnitOfWork"/>.
+        /// </summary>
+        /// <param name="unitOfWork">The unit of work to be used for saving changes.</param>
         public TeamRepository(IUnitOfWork unitOfWork) : base(unitOfWork)
         {
         }
 
+        /// <summary>
+        /// Retrieves all teams, including their team leaders and users.
+        /// </summary>
+        /// <returns>
+        /// An <see cref="IQueryable{Team}"/> representing the collection of all teams, 
+        /// including related team leader and user information.
+        /// </returns>
         public IQueryable<Team> GetTeams()
         {
             return GetDbSet<Team>()
@@ -23,6 +34,14 @@ namespace ASI.Basecode.Data.Repositories
                 .Include(t => t.Users);
         }
 
+        /// <summary>
+        /// Retrieves a team by its unique identifier, including team leader and users.
+        /// </summary>
+        /// <param name="teamId">The unique identifier of the team.</param>
+        /// <returns>
+        /// A <see cref="Team"/> object representing the team with the specified <paramref name="teamId"/>.
+        /// Returns <c>null</c> if the team is not found.
+        /// </returns>
         public Team GetTeamById(int teamId)
         {
             return GetDbSet<Team>()
@@ -31,12 +50,21 @@ namespace ASI.Basecode.Data.Repositories
                 .FirstOrDefault(t => t.TeamId == teamId);
         }
 
+        /// <summary>
+        /// Adds a new team to the database.
+        /// </summary>
+        /// <param name="team">The <see cref="Team"/> object representing the new team to be added.</param>
         public void AddTeam(Team team)
         {
             GetDbSet<Team>().Add(team);
             SaveTeam();
         }
 
+        /// <summary>
+        /// Updates the details of an existing team.
+        /// </summary>
+        /// <param name="team">The <see cref="Team"/> object containing updated information.</param>
+        /// <exception cref="Exception">Thrown if the team is not found in the database.</exception>
         public void UpdateTeam(Team team)
         {
             var existingTeam = GetTeamById(team.TeamId);
@@ -56,6 +84,11 @@ namespace ASI.Basecode.Data.Repositories
             }
         }
 
+        /// <summary>
+        /// Deletes a team by its unique identifier.
+        /// </summary>
+        /// <param name="teamId">The unique identifier of the team to be deleted.</param>
+        /// <exception cref="Exception">Thrown if the team is not found in the database.</exception>
         public void DeleteTeam(int teamId)
         {
             var team = GetTeamById(teamId);
@@ -70,10 +103,13 @@ namespace ASI.Basecode.Data.Repositories
             }
         }
 
+        /// <summary>
+        /// Saves all changes made to the repository.
+        /// </summary>
         public void SaveTeam()
         {
             UnitOfWork.SaveChanges();
         }
     }
-
 }
+
