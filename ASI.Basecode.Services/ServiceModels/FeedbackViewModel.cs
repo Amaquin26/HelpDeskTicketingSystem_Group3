4b55e92c12c7f321d259c78abe0b50575a5f65c9
Rey Aaron Buaya <reycrocs@gmail.com>
2024-10-12 20:22:17 +0800

    xml docs added

--- ASI.Basecode.Services/ServiceModels/FeedbackViewModel.cs
+++ ASI.Basecode.Services/ServiceModels/FeedbackViewModel.cs
@@ -3,26 +3,61 @@ using ASI.Basecode.Services.Validations;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ASI.Basecode.Services.ServiceModels
 {
+    /// <summary>
+    /// Represents a view model for feedback data associated with tickets.
+    /// </summary>
     public class FeedbackViewModel
     {
+        /// <summary>
+        /// Gets or sets the unique identifier for the feedback entry.
+        /// </summary>
         public int FeedbackId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the unique identifier for the associated ticket.
+        /// </summary>
         public int TicketId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the unique identifier for the user who provided the feedback.
+        /// </summary>
         public string UserId { get; set; }
 
+        /// <summary>
+        /// Gets or sets the comment provided by the user.
+        /// Required field with a validation error message.
+        /// </summary>
         [Required(ErrorMessage = "Comment is required.")]
         public string Comment { get; set; }
 
+        /// <summary>
+        /// Gets or sets the rating given by the user.
+        /// Required field with a validation error message.
+        /// </summary>
         [Required(ErrorMessage = "Rating is required.")]
         public int Rating { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the user who created the feedback entry.
+        /// </summary>
         public string CreatedBy { get; set; }
+
+        /// <summary>
+        /// Gets or sets the time when the feedback was created.
+        /// </summary>
         public DateTime CreatedTime { get; set; }
-        public string TicketTitle { get; set; } 
+
+        /// <summary>
+        /// Gets or sets the title of the associated ticket.
+        /// </summary>
+        public string TicketTitle { get; set; }
+
+        /// <summary>
+        /// Gets or sets the list of tickets associated with the feedback.
+        /// </summary>
         public List<Ticket> Tickets { get; set; } = new List<Ticket>();
     }
 }
